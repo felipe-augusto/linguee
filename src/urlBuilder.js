1f6f05425fdb30d71e1e1053c9d7057052de1d3e
Einenlum <yann.rabiller@gmail.com>
2018-08-07 16:00:23 +0200

    Add audio url

--- src/urlBuilder.js
+++ src/urlBuilder.js
@@ -1,29 +1,34 @@
 const urlBuilder = function(config, querystring) {
-  return function(from, to, query) {
-    if (!(from in config.langs)) {
-      throw Error(
-        `"from" parameter (value: "${from}") is not valid. Valid values are: ${Object.keys(
-          config.langs
-        ).join(', ')}`
-      );
-    }
-    if (!(to in config.langs)) {
-      throw Error(
-        `"to" parameter (value: "${to}") is not valid. Valid values are: ${Object.keys(
-          config.langs
-        ).join(', ')}`
-      );
-    }
-    const queryParameters = {
-      ...config.queryParameters,
-      query: query
-    };
+  return {
+    buildUrl: function(from, to, query) {
+      if (!(from in config.langs)) {
+        throw Error(
+          `"from" parameter (value: "${from}") is not valid. Valid values are: ${Object.keys(
+            config.langs
+          ).join(', ')}`
+        );
+      }
+      if (!(to in config.langs)) {
+        throw Error(
+          `"to" parameter (value: "${to}") is not valid. Valid values are: ${Object.keys(
+            config.langs
+          ).join(', ')}`
+        );
+      }
+      const queryParameters = {
+        ...config.queryParameters,
+        query: query
+      };
 
-    const url = `${config.domain}${config.placeholderUrl
-      .replace('{from}', config.langs[from])
-      .replace('{to}', config.langs[to])}`;
+      const url = `${config.domain}${config.placeholderUrl
+        .replace('{from}', config.langs[from])
+        .replace('{to}', config.langs[to])}`;
 
-    return `${url}?${querystring.stringify(queryParameters)}`;
+      return `${url}?${querystring.stringify(queryParameters)}`;
+    },
+    buildAudioUrl: function(path) {
+      return `${config.domain}/mp3/${path}`;
+    }
   };
 };
 
