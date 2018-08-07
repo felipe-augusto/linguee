4745887b332e546076073e8fd5140d13b2f1b0b6
Einenlum <yann.rabiller@gmail.com>
2018-08-07 16:22:02 +0200

    Add iconv to convert ISO-8859-1 response to Utf8

--- src/linguee.js
+++ src/linguee.js
@@ -1,6 +1,7 @@
 const linguee = function(
   cheerio,
   request,
+  iconv,
   config,
   urlBuilder,
   responseTransformer
@@ -10,9 +11,11 @@ const linguee = function(
       const url = urlBuilder.buildUrl(opts.from, opts.to, query);
 
       return new Promise(function(resolve, reject) {
-        request(url, function(error, response, body) {
+        request(url, { encoding: null }, function(error, response, body) {
+          // Linguee encodes the response in ISO-8859-1. We convert it to UTF8
+          const utf8body = iconv.decode(Buffer.from(body), 'ISO-8859-1');
           if (!error && response.statusCode == 200) {
-            const resp = responseTransformer.transform(body, query);
+            const resp = responseTransformer.transform(utf8body, query);
             resolve(resp);
 
             return;
